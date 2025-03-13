3bbcec6f4e32ee6923ac48f8e8c1381e87ca53c6
fedoroi2 <ivan.fedorov2@mail.dcu.ie>
2025-03-13 12:39:39 +0000

    Moved I/O redirection into a separate function in a separate source file

--- Stage2/src/customshell.c
+++ Stage2/src/customshell.c
@@ -13,6 +13,7 @@
 
 #include "parse.h"    // custom file, contains a function that parses strings.
 #include "getpath.h"  // another custom file, contains a function that gets bin path
+#include "ioset.h"
 
 int main (int argc, char ** argv)
 {
@@ -106,66 +107,9 @@ int main (int argc, char ** argv)
             else{
                 continue;                                   // skip to next input if input is newline
             }
-            bool err = 0;                                   // boolean that will be cheched after the loop to see if a file-related error has occured
-            for (int i = 0; i < arg_count; i++)             // look for I/O redirection loop
-            {
-                if (args[i] != NULL)                        // skip if argument is NULL
-                {
-                    if(!strcmp(args[i], ">") && i < (arg_count - 1)){                   // output redirection (truncation)
-                        fflush(stdout);                                                 // flush unwritten data (just in case)
-                        output_desc = open(args[i + 1], 
-                            O_WRONLY | O_CREAT | O_TRUNC, 0600);                        // open file as a descriptor (last argument is permission rights)
-                        
-                        if (output_desc == -1)                                          // check if file was successfully opened
-                        {
-                            printf("Couldn't create file!\n");
-                            err = 1;
-                            break;
-                        }
-                        
-                        dup2(output_desc, STDOUT_FILENO);                               // set stdout to this file
-
-                        args[i] = NULL;                                                 // nullify arguments that are redirection related
-                        args[i + 1] = NULL;
-                        continue;
-                    }
-
-                    else if(!strcmp(args[i], ">>") && i < (arg_count - 1)){             // output redirection (appending)
-                        fflush(stdout);
-                        output_desc = open(args[i + 1], 
-                            O_WRONLY | O_CREAT | O_APPEND, 0600);                       // open file as a descriptor (with O_APPEND as parameter)
-                        
-                        if (output_desc == -1)                                          // check if file was successfully opened
-                        {
-                            printf("Couldn't create file!\n");
-                            err = 1;
-                            break;
-                        }
-                        dup2(output_desc, STDOUT_FILENO);                               // set stdout to this file
-
-                        args[i] = NULL;                                                 // nullify arguments that are redirection related
-                        args[i + 1] = NULL;
-                        continue;
-                    }
-    
-                    else if(!strcmp(args[i], "<") && i < (arg_count - 1)){              // input redirection
-                        fflush(stdin);
-                        input_desc = open(args[i + 1], O_RDONLY);                      // open file as a descriptor
-                        if (input_desc == -1)                                          // check if file was successfully opened
-                        {
-                            printf("File not found!\n");
-                            err = 1;
-                            break;
-                        }
-                
-                        dup2(input_desc, STDIN_FILENO);                                // set stdin to this file
-                        
-                        args[i] = NULL;                                                 // nullify arguments that are redirection related
-                        args[i + 1] = NULL;
-                        continue;
-                    }
-                }
-            }
+            bool err = 
+                setio(arg_count, &args, &output_desc, &input_desc);// boolean that will be cheched after the loop to see if a file-related error has occured
+            
             if (err)                                        // check if there was an error of some kind
             {
                 printf("An error occured when trying to open file.\n");
---
+++ Stage2/src/ioset.h
@@ -0,0 +1,6 @@
+#ifndef IOSET_H
+#define IOSET_H
+#include "parse.h"
+
+bool setio(int arg_count, char*** args, int* output_desc, int* input_desc);
+#endif
